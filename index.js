5d16ce5a4262e20ab3b4dbe6484cd954439c363d
Globik <petrov.ag412@yandex.ru>
2015-09-09 09:22:22 +0300

    awq

--- index.js
+++ index.js
@@ -160,12 +160,13 @@ app.keys=['fg'];
 
  //app.use(session({store:new MongoStore({db:"todo"})}));
  //app.use(session({store:new MongoStore({url:process.env.MONGOHQ_URL_TEST,db:"alikon-fantastic-database"})}));
- app.use(session({store:new MongoStore({url:configDB.url,db:"alikon-fantastic-database"})}));
+ //app.use(session({store:new MongoStore({url:configDB.url,db:"alikon-fantastic-database"})}));
 app.use(session());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(Router(app));
 app.use(bodyParser());
+app.use(session({store:new MongoStore({url:configDB.url,db:"alikon-fantastic-database"})}));
 app.use(flash());
  //iojs index
  app.use(function *(next){this.lowdb=lowdb;yield next;})
