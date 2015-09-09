cf346b4395bedad15e5d61de3ee32911e9e98037
Globik <petrov.ag412@yandex.ru>
2015-09-09 09:40:08 +0300

    config

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports={'localurl':'mongodb://127.0.0.1:27017/todo',
-'simpleloc':'localhost:27017/todo',
+'simpleloc':'localhost:27017/todo','newUrl':'mongodb://alik:123456@dogen.mongohq.com:10004/alikon-fantastic-database',
 'url':process.env.MONGOHQ_URL};
