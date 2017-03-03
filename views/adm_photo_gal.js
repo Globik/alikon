e3e3dfb2f2af7b8beb264014dff65c1f4ccf24c3
Globik <gru5@yandex.ru>
2017-03-03 11:48:44 -0500

    ojkoj

--- views/adm_photo_gal.js
+++ views/adm_photo_gal.js
@@ -22,6 +22,23 @@ ${((buser && buser.role=='superadmin') ? `${admin_main_menu.admin_main_menu({})}
 <style>
 .redChecked{background:red;}
 .orangeUnchecked{background:orange;}
+#article_fotkis{
+background: lightblue;
+display:flex;
+flex-wrap:wrap;
+}
+#fotkis{
+background:lightgreen;
+display:flex;
+flex-wrap:wrap;
+}
+
+.pig,.img_post{
+display:inline-block;
+width:auto;height:auto;
+position:relative;margin:0;
+}
+.
 </style>
 <main id="pagewrap">
 <h4>Add photo gallery</h4>
@@ -31,16 +48,28 @@ ${((buser && buser.role=='superadmin') ? `${admin_main_menu.admin_main_menu({})}
 <b>album id: </b><span id="alb_id"></span><br>
 <b>album title: </b><span id="alb_title"></span><br>
 <hr>
-<button onclick="get_albums();">get album list</button><br>
+<!-- <button onclick="get_albums();">get album list</button><br> -->
+
+<section class="panel"><a href="#popup-img-to-article" title="album lists" onclick="get_albums();">+</a></section>
 <span id="out"></span>
-<ul id="alb_el"></ul>
+<!-- <ul id="alb_el"></ul> -->
 <hr>
 <button id="rmv_unch" style="display:none;" onclick="remove_unchecked()">remove_unchecked</button>
 <hr>
 <button onclick="pics_to_article(this);">add pics to the article</button>
-<div id="fotkis"></div>
+<div id="article_fotkis">${post && post.images ? list_post_imgs(post.images) : "No images yet. Add photos from album."}</div>
+<hr>
+<!-- <button id="to_art_imgs" onclick="add_to_art_imgs();">add pics to post sector</button> -->
+<!-- <div id="fotkis"></div> -->
 <hr>
 <button onclick="pics_to_article(this);">add pics to the article</button>
+
+<a href="#" class="overlay" id="popup-img-to-article"></a>
+<section class="popup"><div style="display:block;background:yellow;position:static;">kuku
+<button id="to_art_imgs" onclick="add_to_art_imgs();">add pics to post sector</button></div>
+<ul id="alb_el"></ul>
+<div id="fotkis"></div>
+</section>
 <script>
 function get_albums(){
 var data={};
@@ -84,29 +113,29 @@ if(fotkis.hasChildNodes()){
 while(fotkis.hasChildNodes()){
 fotkis.removeChild(fotkis.firstChild);
 }}
+if(mata.images !==null){
 mata.images.forEach(function(el,i){
 //var pics=data.fotkis[i];
 	//(foldername,fotkis,basedir,who)
 	 var div=document.createElement('figure');
 	 var basedir="/uploads";
-	 div.className='pig';
+	 div.className='pig schweine';
 	 div.setAttribute('data-picname', el.id);
-	// div.setAttribute("data-src1", el.src1);
- //div.setAttribute("data-src2", el.src2);
-//div.setAttribute("data-src3", el.src3);
-//div.setAttribute("data-src4", el.src4);
-
-	 //div.onclick=function(){alert(this.getAttribute("data-identif"))}
-var str1='<caption>'+[i+1]+'<p class="capitan"></p><input type="checkbox" data-picname='+el.id+' onchange="ckeckboxVal(this)"></caption>';
+	
+var str1='<caption>'+[i+1]+'<p class="capitan"></p><input type="checkbox" data-picname='+el.id+' data-me="me" onchange="ckeckboxVal(this)"></caption>';
 var str2='<div class="img-cont"><img src="'+basedir+'/'+el.src1+'"></div>';
-var str3='<figcaption data-src1="'+el.src1+'" data-src2="'+el.src2+'" data-src3="'+el.src3+'" data-src4="'+el.src4+'"><span class="p-descr" contenteditable=true>Description</span></br>';
+var str3='<figcaption data-picnami="'+el.id+'" data-src1="'+el.src1+'" data-src2="'+el.src2+'" data-src3="'+el.src3+'" data-src4="'+el.src4+'">';
+var str6='<textarea style="width:98%;" class="p-descr" placeholder="description">'+el.content+'</textarea></br>';
+
 var str4='<span class="p-title" contenteditable=true>Title</span><br>';
-var palt='<span class="p-alt" contenteditable=true>alt</span><br>';
+var palt='<textarea class="p-alt" placeholder="alt" style="width:98%;">'+el.alt+'</textarea><br>';
 var pquelle='<span class="p-quelle" contenteditable=true>quelle</span>';
 var str5='</figcaption>';
-	 div.innerHTML=[str1,str2,str3,str4,palt,pquelle,str5].join('');
-	 docfr.appendChild(div);
+div.innerHTML=[str1,str2,str3,str6,str4,palt,pquelle,str5].join('');
+docfr.appendChild(div);
 })
+
+}
 fotkis.appendChild(docfr);
 doit();
 }else{alert(this.response);}
@@ -120,29 +149,25 @@ function doit(){
 var cols_=document.querySelectorAll('.pig');
 var dragSrcEl_ = null;
 handleDragStart = function(e) {
-    e.dataTransfer.effectAllowed = 'move';
-    e.dataTransfer.setData('text/html', this.innerHTML);
-//me
-//e.dataTransfer.setData('text/html2',this.outerHTML);
-	dragSrcEl_ = this;
+e.dataTransfer.effectAllowed = 'move';
+e.dataTransfer.setData('text/html', this.innerHTML);
+dragSrcEl_ = this;
 };
 handleDragOver = function(e) {
-    if (e.preventDefault) {
-      e.preventDefault(); 
-    }
+if (e.preventDefault) {
+e.preventDefault(); 
+}
 e.dataTransfer.dropEffect = 'move';
 return false;
   };
 handleDrop = function(e) {
+if (e.stopPropagation) {
+e.stopPropagation();
+}
+if (dragSrcEl_ != this && dragSrcEl_ !=null) {
+dragSrcEl_.innerHTML = this.innerHTML;
 
-    if (e.stopPropagation) {
-      e.stopPropagation();
-    }
-    if (dragSrcEl_ != this) {
-      dragSrcEl_.innerHTML = this.innerHTML;
-//dragSrcEl_.outerHTML=this.outerHTML;
-      this.innerHTML = e.dataTransfer.getData('text/html');
-//this.outerHTML=e.dataTransfer.getData('text/html2');
+this.innerHTML = e.dataTransfer.getData('text/html');
     }
 return false;
   };
@@ -162,38 +187,81 @@ return false;
     col.addEventListener('dragend', handleDragEnd, false);
   });
 }
+function ckeckboxVal2(el){
+//el.parentNode.className="fucker";
+el.parentNode.remove();
+}
+doit();
 function ckeckboxVal(el){
 //alert(el.checked);
 var buka=el.getAttribute('data-picname');
+//var suka=getDomArray('.pig.schweine');
 //alert('data-picname: '+buka);
-var rmvs=getDomArray('[data-picname="'+buka+'"]');
+var rmvs=getDomArray('#fotkis [data-picname="'+buka+'"]');
 rmvs.forEach(function(l,i){
 //rmvs[i].remove();
-if(el.checked){
-
-l.className="redChecked";
-go_orange();}
-else{l.className="orangeUnchecked";rmv_unch.style.display="block";}
+  if(el.checked){
+el.parentNode.className="redChecked";
+go_orange();
+}
+else{
+l.className="orangeUnchecked";
+rmv_unch.style.display="block";}
 });
 }
+
 function go_orange(){
-var ori=getDomArray('.pig');
+var ori=getDomArray('.pig.schweine');
 ori.forEach(function(el,i){
 el.className="orangeUnchecked";
 });
 rmv_unch.style.display="block";
 }
-function remove_unchecked(el){
+
+
+function remove_unchecked(){
 //alert('ok')
 var rmvs=getDomArray('.orangeUnchecked');
 rmvs.forEach(function(el,i){
 rmvs[i].remove();
 });
 rmv_unch.style.display="none";
+//add_to_art_imgs();
+//fotkis.style.display="none";
+}
+
+
+
+function add_to_art_imgs(){
+var ed=getDomArray('.redChecked');
+
+remove_unchecked();
+ed.forEach(function(el,i){
+article_fotkis.appendChild(ed[i].cloneNode(true));
+})
+
+boit();
+}
+
+function boit(){
+var bed=getDomArray('.redChecked');
+bed.forEach(function(el,i){
+bed[i].className="pig";
+})
+doit();
+
+if(fotkis.hasChildNodes()){
+while(fotkis.hasChildNodes()){
+fotkis.removeChild(fotkis.firstChild);
+}}
 }
 
 function pics_to_article(){
 var kasak=true;
+if(fotkis.hasChildNodes()){
+while(fotkis.hasChildNodes()){
+fotkis.removeChild(fotkis.firstChild);
+}}
 var pTitle=getDomArray('.p-title');
 //var pSrc1=getDomArray('.srcset1');
 
@@ -221,6 +289,7 @@ pSrc4=getDomArray('.srcset4');
 	var data={};
 	data.images=[];
 	data.article_id=article_id.textContent;//"345";
+    
 	//alert(dataId);
 	if(article_id.textContent !==undefined){
 	
@@ -230,9 +299,10 @@ pSrc4=getDomArray('.srcset4');
 
 	data.images.push({
     title:pTitle[k].innerHTML,
-    content: pDescr[k].innerHTML,
-    alt: pAlt[k].textContent,
+    content: pDescr[k].value,
+    alt: pAlt[k].value,
     quelle:pQuelle[k].textContent,
+    id:pSrc1[k].dataset.picnami,
     src1: pSrc1[k].dataset.src1,
 	src2: pSrc1[k].dataset.src2,
     src3: pSrc1[k].dataset.src3,  
@@ -262,7 +332,7 @@ pSrc4=getDomArray('.srcset4');
 	 }}
 	 //data._id="123456";
 	 //alert(JSON.stringify(data.images));
- xhr.send(JSON.stringify(data));
+     xhr.send(JSON.stringify(data));
 	
 	 } 
 	 else{alert('datid not fullfilled');}
@@ -282,3 +352,21 @@ return elar;
 </html>`;
 }
 module.exports={adm_photo_gal};
+
+function list_post_imgs(n){
+let s='';
+//console.log('n: ',n);
+n.forEach((el,i)=>{
+s+=`<figure class="pig">
+<caption>${[i+1]}<p class="capitan"></p><input type="checkbox" data-picname="${el.id}" onchange="ckeckboxVal2(this)"/></caption>
+<div class="img-cont"><img src="/uploads/${el.src1}"></div>
+<figcaption data-picnami="${el.id}" data-src1="${el.src1}" data-src2="${el.src2}" data-src3="${el.src3}" data-src4="${el.src4}">
+<textarea class="p-descr" style="width:98%;" placeholder="description">${el.content}</textarea><br>
+<span class="p-title" contenteditable=true>${el.title}</span><br>
+<textarea class="p-alt" placeholder="alt" style="width:98%;">${el.alt}</textarea><br>
+<span class="p-quelle" contenteditable=true>${el.quelle}</span>
+</figcaption>
+</figure>`;
+})
+return s;
+}
