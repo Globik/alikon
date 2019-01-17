6a019bf8d19f247215ade7c2527c7a81ae79c2b0
Globik <gru5@yandex.ru>
2019-01-17 14:45:47 -0500

    single handler

--- views/test_mouse.js
+++ views/test_mouse.js
@@ -27,95 +27,78 @@ background:none;
 </style>
 </head>
 <body>
-<h4>mouse down / up</h4>
-<span id="up">up</span>
+<h4>mouse down / up and touchstart / touchend</h4>
+<span id="up" data-typ="up">up</span>
 <output id="out"></output>
-<span id="down">down</span>
+<span id="down" data-typ="down">down</span>
 
-<h4>touch enter / leave</h4>
-<span id="up2">up</span>
+<h4>only touchstart / touchend</h4>
+<span id="up2" data-typ="up">up</span>
 <output id="out2"></output>
-<span id="down2">down</span>
+<span id="down2" data-typ="down">down</span>
 <h4>type</h4>
 <div id="out4"></div>
 </body>
 <script>
 var m;var int=0;var out4=gid('out4'),out2=gid('out2'),out=gid('out'),up=gid('up'),up2=gid('up2'),down=gid('down'),down2=gid('down2');
 var s_timer=100;
-up.addEventListener('mousedown',function(ev){
+
+function handler_plus(ev){
 ev.preventDefault();
 ev.stopPropagation();
 out4.textContent=ev.type;
+var typ=ev.target.getAttribute("data-typ");
+console.log("typ: ",typ);
+if(ev.type=="mousedown"){
 m=true;
-evi_plus();	
-},false)
-up.addEventListener('mouseup',function(ev){
-ev.preventDefault();
-ev.stopPropagation();
-out4.textContent=ev.type;
+typ=="up"?evi_plus():evi_minus();	
+}else if(ev.type=="mouseup"){
+m=false;
+}else if(ev.type=="touchstart"){
+m=true;
+evi_plus();
+}else if(ev.type=="touchend"){
 m=false;	
-},false);
+}	
+}
+up.addEventListener('mousedown',handler_plus,false);
+up.addEventListener('mouseup', handler_plus,false);
+down.addEventListener('mousedown', handler_plus,false);
+down.addEventListener('mouseup', handler_plus,false);
+
+up.addEventListener('touchstart', handler_plus,false);
+up.addEventListener('touchend', handler_plus,false);
+down.addEventListener('touchstart', handler_plus,false);
+down.addEventListener('touchend', handler_plus,false);
 
 function evi_plus(){
 if(m){
+int++;
 out.textContent=int;
 out2.textContent=int;
 setTimeout("evi_plus()",s_timer);
-int++;
+
 }else{return false;}
 }
 
 
 function evi_minus(){
 if(m){
+int--;
 out.textContent=int;
 out2.textContent=int;
 setTimeout("evi_minus()",s_timer);
-int--;
+
 }else{return false;}
 }
 
-
-down.addEventListener('mousedown',function(ev){
-ev.preventDefault();
-ev.stopPropagation();
-out4.textContent=ev.type;
-m=true;
-evi_minus();	
-},false)
-down.addEventListener('mouseup',function(ev){
-ev.preventDefault();
-ev.stopPropagation();
-out4.textContent=ev.type;
-m=false;	
-},false)
-
 /* touch test */
-up2.addEventListener('touchstart', function(ev){
-ev.preventDefault();
-ev.stopPropagation();
-out4.textContent=ev.type;
-m=true;
-evi_plus();	
-},false)
-up2.addEventListener('touchend', function(ev){
-ev.preventDefault();
-ev.stopPropagation();
-out4.textContent=ev.type;
-m=false;	
-},false);
 
-down2.addEventListener('touchstart', function(ev){
-ev.preventDefault();
-ev.stopPropagation();
-out4.textContent=ev.type;
-m=true;
-evi_minus();	
-},false)
-down2.addEventListener('touchend', function(ev){
-out4.textContent=ev.type;
-m=false;	
-},false);
+up2.addEventListener('touchstart', handler_plus,false);
+up2.addEventListener('touchend', handler_plus,false);
+
+down2.addEventListener('touchstart', handler_plus,false);
+down2.addEventListener('touchend', handler_plus,false);
 
 function gid(id){return document.getElementById(id);}
 </script>
